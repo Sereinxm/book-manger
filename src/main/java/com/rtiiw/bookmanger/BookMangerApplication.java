bcac3e82980e85a2ba62862a3c7b36a1b8a20d3e
cxm <dsadas@dsa.com>
2025-07-18 12:49:28 +0800

    init

---
+++ src/main/java/com/rtiiw/bookmanger/BookMangerApplication.java
@@ -0,0 +1,13 @@
+package com.rtiiw.bookmanger;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BookMangerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BookMangerApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/rtiiw/bookmanger/BookMangerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rtiiw.bookmanger;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BookMangerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
